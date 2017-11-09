bc2e97ebb9c79e99b4da588fc2f5222499bf1861
valdasvabolis <thisisvaldasvabolis@gmail.com>
2017-11-09 04:07:58 +0200

    Fixed form (now uses helper), controller receives body and title

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -7,7 +7,8 @@ class PollsController < ApplicationController
     options = params[:poll][:poll_options_string].split(/\s+/).map do |opt|
       PollOption.new(title: opt)
     end
-
+    poll_title = params[:title]
+    poll_body = params[:body]
     poll = Poll.new(poll_options: options)
 
     poll.save
